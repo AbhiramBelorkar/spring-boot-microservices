b6deb2c714945b849b47f64a4517c02a6deef624
Abhiram Belorkar <abhirambelorkar@Abhirams-MacBook-Air.local>
2025-06-30 09:06:29 +0530

    discovery server

--- microservices-connect/discovery-server/src/main/java/com/microservices/discovery/DiscoveryServerApplication.java
+++ microservices-connect/discovery-server/src/main/java/com/microservices/discovery/DiscoveryServerApplication.java
@@ -1,5 +1,6 @@
 package com.microservices.discovery;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
@@ -10,4 +11,11 @@ public class DiscoveryServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(DiscoveryServerApplication.class, args);
     }
+
+    @PostConstruct
+    public void init() {
+        System.out.println("Eureka Server started with port: " + System.getProperty("server.port"));
+    }
 }
+
+
--- microservices-connect/order-service/src/main/java/com/microservices/order/service/OrderService.java
+++ microservices-connect/order-service/src/main/java/com/microservices/order/service/OrderService.java
@@ -20,7 +20,7 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
-    private final WebClient webClient;
+    private final WebClient.Builder webClientBuilder;
 
     public void placeOrder(OrderRequest orderRequest){
         Order order = new Order();
@@ -38,8 +38,8 @@ public class OrderService {
                 .toList();
 
 //        Call inventory service and place order if product is in stock
-        InventoryReponse[] inventoryReponsesArray = webClient.get()
-                .uri("http://localhost:8082/api/inventory",
+        InventoryReponse[] inventoryReponsesArray = webClientBuilder.build().get()
+                .uri("http://inventory-service/api/inventory",
                         uriBuilder -> uriBuilder.queryParam("skuCode", skuCode).build())
                 .retrieve()
                 .bodyToMono(InventoryReponse[].class)
