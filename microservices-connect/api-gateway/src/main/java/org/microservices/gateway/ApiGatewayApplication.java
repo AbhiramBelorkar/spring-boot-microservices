bae2a7bf3ed6ef6ca790bcbfcc4f9a30452c0317
abhiramb <abhiramb@cdac.in>
2025-07-01 11:41:20 +0530

    api-gateway update

---
+++ microservices-connect/api-gateway/src/main/java/org/microservices/gateway/ApiGatewayApplication.java
@@ -0,0 +1,14 @@
+package org.microservices.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.swing.*;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class,args);
+    }
+}
